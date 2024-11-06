c741d820ce7640cb61bc2de8230ffb1d2ae8a14b
jpablopachar <jpablopachar1993@gmail.com>
2024-11-06 17:21:20 -0500

    Diseño e implementación de lógica para footer

--- src/app/components/footer.component.ts
+++ src/app/components/footer.component.ts
@@ -1,13 +1,54 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  Signal,
+} from '@angular/core'
+import { Router, RouterLink } from '@angular/router'
+import { UserInfo } from '@app/models'
+import { selectUserInfo } from '@app/store/auth'
+import { selectCardProductCount, selectWishlistCount } from '@app/store/card'
+import {
+  FontAwesomeModule,
+  IconDefinition,
+} from '@fortawesome/angular-fontawesome'
+import {
+  faFacebookF,
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons'
+import { faCartShopping, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { Store } from '@ngrx/store'
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, FontAwesomeModule, RouterLink],
   templateUrl: './footer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent { }
+export class FooterComponent {
+  private readonly _store = inject(Store);
+  private readonly _router: Router = inject(Router);
+
+  public $userInfo: Signal<UserInfo | null> =
+    this._store.selectSignal(selectUserInfo);
+  public $cardProductCount: Signal<number> = this._store.selectSignal(
+    selectCardProductCount
+  );
+  public $wishlistCount: Signal<number> =
+    this._store.selectSignal(selectWishlistCount);
+
+  public faFacebookF: IconDefinition = faFacebookF;
+  public faTwitter: IconDefinition = faTwitter;
+  public faLinkedin: IconDefinition = faLinkedin;
+  public faGithub: IconDefinition = faGithub;
+  public faCartShopping: IconDefinition = faCartShopping;
+  public faHeart: IconDefinition = faHeart;
+
+  public navigate(path: string): void {
+    this._router.navigate([path]);
+  }
+}
