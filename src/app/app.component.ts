13a210ea3dc2d45b4e724768a8c62199698c2f07
jpablopachar <jpablopachar1993@gmail.com>
2024-11-05 21:18:38 -0500

    Se inicia la App obteniendo categorías

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core'
+import { Component, inject, OnInit } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { Store } from '@ngrx/store'
+import { homeActions } from './store/home'
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,10 @@ import { RouterOutlet } from '@angular/router'
   imports: [RouterOutlet],
   template: '<router-outlet />',
 })
-export class AppComponent {
-  title = 'multi-vendor-frontend-angular';
+export class AppComponent implements OnInit {
+  private readonly _store = inject(Store);
+
+  ngOnInit() {
+    this._store.dispatch(homeActions.getCategories());
+  }
 }
--- src/app/components/header.component.ts
+++ src/app/components/header.component.ts
@@ -1,13 +1,24 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import {
+  IconDefinition,
+  faArrowDown,
+  faHeart,
+  faPhone,
+  faShoppingCart,
+} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, FontAwesomeModule,],
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+  public faPhone: IconDefinition = faPhone;
+  public faArrowDown: IconDefinition = faArrowDown;
+  public faHeart: IconDefinition = faHeart;
+  public faShoppingCart: IconDefinition = faShoppingCart;
+}
