fffb73e67939e82b33664d0a315b884e7d566755
jpablopachar <jpablopachar1993@gmail.com>
2024-11-08 18:12:52 -0500

    Diseño aplicado a ShopComponent

--- src/app/components/products/shop-products.component.ts
+++ src/app/components/products/shop-products.component.ts
@@ -1,13 +1,100 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  Input,
+  InputSignal,
+} from '@angular/core'
+import { ProductInfo } from '@app/models'
+import {
+  FontAwesomeModule,
+  IconDefinition,
+} from '@fortawesome/angular-fontawesome'
+import { faEye, faHeart } from '@fortawesome/free-regular-svg-icons'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { RatingComponent } from '../rating.component'
+
+export type StylesType = 'grid' | 'list';
 
 @Component({
   selector: 'app-shop-products',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  template: `<p>shop-products works!</p>`,
+  imports: [CommonModule, FontAwesomeModule, RatingComponent],
+  template: `
+    <div
+      class="w-full grid gap-3"
+      [ngClass]="
+        styles === 'grid'
+          ? 'grid-cols-3 md-lg:grid-cols-2 md:grid-cols-2'
+          : 'grid-cols-1 md-lg:grid-cols-2 md:grid-cols-2'
+      "
+    >
+      @for (product of $products(); track product._id;) {
+      <div
+        class="flex transition-all duration-1000 hover:shadow-md hover:-translate-y-3 w-full gap-4 bg-white p-1 rounded-md"
+        [ngClass]="
+          styles === 'grid'
+            ? 'flex-col justify-start items-start'
+            : 'justify-start items-center md-lg:flex-col md-lg:justify-start md-lg:items-start'
+        "
+      >
+        <div
+          [ngClass]="
+            styles === 'grid'
+              ? 'w-full relative group h-[210px] md:h-[270px] xs:h-[170px] overflow-hidden'
+              : 'md-lg:w-full relative group h-[210px] md:h-[270px] overflow-hidden'
+          "
+        >
+          <img
+            class="h-[240px] rounded-md md:h-[270px] xs:h-[170px] w-full object-cover"
+            [src]="product.images[0]"
+            alt=""
+          />
+          <ul
+            class="flex transition-all duration-700 -bottom-10 justify-center items-center gap-2 absolute w-full group-hover:bottom-3"
+          >
+            <li
+              class="w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all"
+            >
+              <fa-icon [icon]="faHeart"></fa-icon>
+            </li>
+            <li
+              class="w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all"
+            >
+              <fa-icon [icon]="faEye"></fa-icon>
+            </li>
+            <li
+              class="w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all"
+            >
+              <fa-icon [icon]="faShoppingCart"></fa-icon>
+            </li>
+          </ul>
+        </div>
+        <div class="flex justify-start items-start flex-col gap-1">
+          <h2 class="font-bold">{{ product.name }}</h2>
+          <div class="flex justify-start items-center gap-3">
+            <span class="text-md font-semibold">{{
+              product.price | currency
+            }}</span>
+            <div class="flex">
+              <app-rating [ratings]="product.rating"></app-rating>
+            </div>
+          </div>
+        </div>
+      </div>
+      }
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShopProductsComponent { }
+export class ShopProductsComponent {
+  @Input({ required: true }) styles!: StylesType;
+
+  readonly $products: InputSignal<ProductInfo[]> =
+    input.required<ProductInfo[]>();
+
+  public faEye: IconDefinition = faEye;
+  public faHeart: IconDefinition = faHeart;
+  public faShoppingCart: IconDefinition = faShoppingCart;
+}
--- src/app/pages/shops.component.ts
+++ src/app/pages/shops.component.ts
@@ -1,13 +1,107 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgxSliderModule, Options } from '@angular-slider/ngx-slider'
+import { CommonModule } from '@angular/common'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core'
+import { Router } from '@angular/router'
+import {
+  FooterComponent,
+  HeaderComponent,
+  PaginationComponent,
+  ProductsComponent,
+} from '@app/components'
+import { Category, ProductInfo } from '@app/models'
+import {
+  selectCategories,
+  selectLatestProduct,
+  selectProducts,
+} from '@app/store/home'
+import {
+  FontAwesomeModule,
+  IconDefinition,
+} from '@fortawesome/angular-fontawesome'
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
+import {
+  faArrowRight,
+  faGrin,
+  faList,
+  faStar as fasStar,
+} from '@fortawesome/free-solid-svg-icons'
+import { Store } from '@ngrx/store'
+
+const SLIDER_OPTIONS: Options = {
+  floor: 0,
+  ceil: 1000,
+  step: 5,
+  showTicks: false,
+  animate: false,
+  showSelectionBar: true,
+  translate: (value: number): string => {
+    return `$${value}`;
+  },
+};
 
 @Component({
   selector: 'app-shops',
   standalone: true,
   imports: [
     CommonModule,
+    FontAwesomeModule,
+    NgxSliderModule,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent,
+    ShopsComponent,
+    PaginationComponent,
   ],
   templateUrl: './shops.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShopsComponent { }
+export class ShopsComponent {
+  private readonly _store = inject(Store);
+  private readonly _router: Router = inject(Router);
+
+  public $products: Signal<ProductInfo[]> =
+    this._store.selectSignal(selectProducts);
+  public $categories: Signal<Category[]> =
+    this._store.selectSignal(selectCategories);
+  /* public $products =
+    this._store.selectSignal(selectPri); */
+  public $latestProduct: Signal<ProductInfo[][]> =
+    this._store.selectSignal(selectLatestProduct);
+  /* public $products =
+    this._store.selectSignal(selectTota); */
+  /* public $products =
+    this._store.selectSignal(selectP); */
+
+  public $filter: WritableSignal<boolean> = signal(true);
+  public $styles: WritableSignal<string> = signal('grid');
+  public $rating: WritableSignal<number | string> = signal('');
+  public $sortPrice: WritableSignal<string> = signal('');
+  public $category: WritableSignal<string> = signal('');
+  public $state: WritableSignal<{ values: number[] }> = signal({
+    values: [0, 100],
+  });
+
+  public sliderOptions: Options = SLIDER_OPTIONS;
+  public faStar: IconDefinition = fasStar;
+  public farStar: IconDefinition = farStar;
+  public faArrowRight: IconDefinition = faArrowRight;
+  public faGrin: IconDefinition = faGrin;
+  public faList: IconDefinition = faList;
+
+  public queryCategory(event: Event, value: string): void {
+    const target = event.target as HTMLInputElement;
+
+    if (target.checked) {
+      this.$category.set(value);
+    } else {
+      this.$category.set('');
+    }
+  }
+}
