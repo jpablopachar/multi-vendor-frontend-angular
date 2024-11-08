c22f00155372570b5ca0417b62edcedd0bf6659b
jpablopachar <jpablopachar1993@gmail.com>
2024-11-07 19:04:50 -0500

    Ajuste de breakpoints para configuración de tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    screens: {
+      xl: { max: "1200px" },
+      lg: { max: "1080px" },
+      "md-lg": { max: "991px" },
+      md: { max: "768px" },
+      sm: { max: "576px" },
+      xs: { max: "480px" },
+      "2xs": { max: "340px" },
+    },
   },
   plugins: [],
-}
-
+};
